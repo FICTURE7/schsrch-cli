e4f7a56d71eb2ce3e6b730f4b0af65a255ba6997
FICTURE7 <ficture7@gmail.com>
2018-06-24 21:29:51 +0400

    lib: refactor stuff

--- bin/schsrch-dl.js
+++ bin/schsrch-dl.js
@@ -1,1 +1,15 @@
 const schsrch = require('schsrch-js');
+
+schsrch.get().subjects().then((val) => {
+	console.log(val);
+});
+
+schsrch.get({
+	id: 9702,
+	season: 'w',
+	year: 18
+}).then((val) => {
+	console.log(val);
+}).catch((err) => {
+	console.log(err);
+});
