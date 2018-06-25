730c22da0b1a0756e35b180375e6c66ce45a79b3
FICTURE7 <ficture7@gmail.com>
2018-06-25 20:12:25 +0400

    other: renamed to schsrch-cli

--- bin/schsrch-dl.js
+++ bin/schsrch-dl.js
@@ -1,15 +1,16 @@
 const schsrch = require('schsrch-js');
+const argv = require('yargs')
+	.command(['list', 'ls'], 'list all subjects', () => {}, (argv) => {
+		schsrch.get().subjects().then((subjects) => {
+			for (let subject of subjects) {
+				/* subject with id 8683 has a name whose value is null */
+				if (subject.name != null) {
+					console.log(subject.name);
+				}
+			}
+		}).catch((err) => {
+			process.exit(1);
+		});
+	})
+	.argv;
 
-schsrch.get().subjects().then((val) => {
-	console.log(val);
-});
-
-schsrch.get({
-	id: 9702,
-	season: 'w',
-	year: 18
-}).then((val) => {
-	console.log(val);
-}).catch((err) => {
-	console.log(err);
-});
